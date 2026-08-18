0dd01745f4255c9932afe6b0424bbee9a1b3e72c
Asmual <Asmual04@gmail.com>
2026-08-18 22:49:20 +0600

    ADDED:Product Store File

---
+++ src/store/useProductStore.ts
@@ -0,0 +1,68 @@
+import { create } from 'zustand';
+
+export interface Product {
+  _id: string;
+  title: string;
+  slug: string;
+  description: string;
+  category: string;
+  brand: string;
+  price: number;
+  discountPrice?: number;
+  stock: number;
+  images: string[];
+  vendorName: string;
+  isFlashSale: boolean;
+  flashSaleDiscountPercent?: number;
+  averageRating: number;
+  totalReviews: number;
+  tags: string[];
+}
+
+interface ProductFilterState {
+  search: string;
+  category: string;
+  brand: string;
+  minPrice: number;
+  maxPrice: number;
+  minRating: number;
+  sortBy: string;
+  isFlashSale: boolean;
+  setSearch: (search: string) => void;
+  setCategory: (category: string) => void;
+  setBrand: (brand: string) => void;
+  setPriceRange: (min: number, max: number) => void;
+  setMinRating: (rating: number) => void;
+  setSortBy: (sortBy: string) => void;
+  setIsFlashSale: (isFlashSale: boolean) => void;
+  resetFilters: () => void;
+}
+
+export const useProductStore = create<ProductFilterState>((set) => ({
+  search: '',
+  category: '',
+  brand: '',
+  minPrice: 0,
+  maxPrice: 2000,
+  minRating: 0,
+  sortBy: 'newest',
+  isFlashSale: false,
+  setSearch: (search) => set({ search }),
+  setCategory: (category) => set({ category }),
+  setBrand: (brand) => set({ brand }),
+  setPriceRange: (minPrice, maxPrice) => set({ minPrice, maxPrice }),
+  setMinRating: (minRating) => set({ minRating }),
+  setSortBy: (sortBy) => set({ sortBy }),
+  setIsFlashSale: (isFlashSale) => set({ isFlashSale }),
+  resetFilters: () =>
+    set({
+      search: '',
+      category: '',
+      brand: '',
+      minPrice: 0,
+      maxPrice: 2000,
+      minRating: 0,
+      sortBy: 'newest',
+      isFlashSale: false,
+    }),
+}));
