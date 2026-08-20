00b807edbd34e4884badcba53dd1d0ab3b07de65
Asmual Obaidul Hoque <73558650+Asmual@users.noreply.github.com>
2026-08-20 13:17:17 +0600

    MODIFIED: Add SHopNexus Logo in public/images

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useState, FormEvent } from 'react';
 import Link from 'next/link';
 import { usePathname, useRouter } from 'next/navigation';
 import { useCartStore } from '@/store/useCartStore';
