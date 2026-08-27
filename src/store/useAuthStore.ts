97d2168675b829adc4d47979ef66e215974542aa
S.M. Amirul Islam Saad <Saad0174742@gmail.com>
2026-08-27 15:58:54 +0600

    [UPDATED]: Clean User interface schema with nexusCoins field in useAuthStore

--- src/store/useAuthStore.ts
+++ src/store/useAuthStore.ts
@@ -13,10 +13,11 @@ export interface User {
   address?: string;
   city?: string;
   zipCode?: string;
-  nexusCoins?: number;
   country?: string;
   storeName?: string;
-  storeDescription?: string;  loginStreak?: number;
+  storeDescription?: string;
+  nexusCoins?: number;
+  loginStreak?: number;
   lastVisitDate?: string;
   isVipMember?: boolean;
   vipFirstOrderUsed?: boolean;
