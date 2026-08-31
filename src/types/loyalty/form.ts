46450982ceeffb35d4d7a20fe088ba8ffa43f3b6
S.M. Amirul Islam Saad <Saad0174742@gmail.com>
2026-08-31 10:25:00 +0600

    [ADDED]: Combo bundle deal builder form schema with live discount calculation

--- src/types/loyalty/form.ts
+++ src/types/loyalty/form.ts
@@ -1,6 +1,2 @@
-export interface ICreateBundleForm {
-  title: string;
-  badgeText: string;
-  selectedProductIds: string[];
-  discountPercentage: number;
-}
+// Bundle Creation Form
+export interface ICreateBundleForm { title: string; badgeText: string; selectedProductIds: string[]; discountPercentage: number; }
