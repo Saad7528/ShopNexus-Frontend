51c08ceb7ab6013c3937a04eaa4943e8233995eb
S.M. Amirul Islam Saad <Saad0174742@gmail.com>
2026-08-31 08:45:00 +0600

    [ADDED]: 8-domain granular security permissions toggle matrix data models

--- src/types/staff/permissions.ts
+++ src/types/staff/permissions.ts
@@ -1,9 +1,2 @@
-export interface ISecurityDomainPermission {
-  domainId: string;
-  domainName: string;
-  description: string;
-  superAdmin: boolean;
-  customerCare: boolean;
-  inventory: boolean;
-  logistics: boolean;
-}
+// Staff Permissions Matrix Model
+export interface ISecurityDomainPermission { domainId: string; domainName: string; description: string; superAdmin: boolean; customerCare: boolean; inventory: boolean; logistics: boolean; }
