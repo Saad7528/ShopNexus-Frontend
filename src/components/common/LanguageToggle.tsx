e1cce7115106e96f94d052f20e61b05295efa2f6
saikot05 <saikotislam08@gmail.com>
2026-09-02 23:29:53 +0600

    [UPDATED]: language toggle add alignment classes to outer div

--- src/components/common/LanguageToggle.tsx
+++ src/components/common/LanguageToggle.tsx
@@ -25,7 +25,7 @@ export const LanguageToggle: React.FC<LanguageToggleProps> = ({
     return (
       <div
         className={twMerge(
-          'p-2 sm:p-2.5 rounded-xl bg-slate-100 dark:bg-slate-900/70 border border-slate-200 dark:border-slate-800 text-slate-400 text-xs font-bold',
+          'p-2 sm:p-2.5 rounded-xl bg-slate-100 dark:bg-slate-900/70 border border-slate-200 dark:border-slate-800 text-slate-400 text-xs font-bold items-center justify-center',
           className
         )}
       >
